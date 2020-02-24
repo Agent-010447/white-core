08c7a6ffe5290c46aed38cee40967c7a69e88243
Agent_040147 <50374203+Agent-010447@users.noreply.github.com>
2020-02-24 06:08:59 +0100

    Add files via upload

---
+++ whitecore.java
@@ -0,0 +1,54 @@
+package fr.agent040147.whitecore;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+
+
+public class whitecore extends JavaPlugin implements Listener {
+
+
+    public static ArrayList<Player> spying = new ArrayList<>();
+
+    @Override
+    public void onEnable() {
+        System.out.println("White-Core ON");
+
+        Bukkit.getPluginManager().registerEvents(this, this);
+
+        getCommand("spy").setExecutor(new CommandSpy());
+
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        System.err.println("White-Core OFF");
+        super.onDisable();
+    }
+
+
+    @EventHandler
+
+    public void detectlacommande(PlayerCommandPreprocessEvent e) {
+        //perm ignor spy whitecore.spy.ignore
+
+        if ((e.getPlayer().hasPermission("whitecore.spy.ignore") && !(e.getPlayer().isOp())) || (e.getPlayer().hasPermission("whitecore.spy.ignore") && (e.getPlayer().isOp())))
+            return;
+
+        //�coute la console des joueurs
+
+
+        for (Player p : whitecore.spying) {
+
+            p.sendMessage(e.getPlayer().getName() + ": " + e.getMessage());
+
+
+        }
+    }
+}
